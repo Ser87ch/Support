e08691c3417c0026de463195aebf2aa1b8d591e8
Ser <Chernobrivenko@tehnosk.ru>
2012-05-12 18:37:48 +0400

    get operdate

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,7 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		PayDoc pd = new PayDoc();
-		pd.plat = new PayDoc.Client("040305000", "40102810000000010001");
-		
-		pd.plat.contrrazr();
-		
-		System.out.println(pd.plat.ls);
+		PayDocList pl = new PayDocList();
 	}
 
 }
--- src/PayDocList.java
+++ src/PayDocList.java
@@ -1,7 +1,7 @@
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.lang.System;
 
 public class PayDocList {
 	private List<PayDoc> pdl;
@@ -11,17 +11,22 @@ public class PayDocList {
 			DB db = new DB(Settings.server, Settings.db, Settings.user, Settings.pwd);
 			db.connect();
 			
-			ResultSet rs = db.st.executeQuery("select VALUE from dbo.XDM_CONF_SETTINGS where ID_PARAM = 1004");
+			//получение папки сабс
+			//ResultSet rs = db.st.executeQuery("select VALUE from dbo.XDM_CONF_SETTINGS where ID_PARAM = 1004");
 		
-			pdl = new ArrayList<>();
-
-			for(int i = 0; i < Settings.GenDoc.numBIK; i++) {
-				
-
-				for(int j = 0; j < Settings.GenDoc.numberDoc; j++)	{
-
-				}
-			}
+			ResultSet rs = db.st.executeQuery("SELECT top 1 OPER_DATE as dt FROM XDM_OPERDAY_PROP WHERE VALUE = 0");
+			rs.next();
+			Date date = rs.getDate("dt");
+			System.out.println(date.toString());
+//			pdl = new ArrayList<>();
+//
+//			for(int i = 0; i < Settings.GenDoc.numBIK; i++) {
+//				
+//
+//				for(int j = 0; j < Settings.GenDoc.numberDoc; j++)	{
+//
+//				}
+//			}
 			
 			db.close();
 		} catch(Exception e) {
